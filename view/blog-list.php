c9ac6ae153371f0caa9140c85d280b64a1c9f5e3
tienbotay <77239325+tienbotay@users.noreply.github.com>
2021-11-29 15:43:15 +0700

    sửa trang tin tức

--- view/blog-list.php
+++ view/blog-list.php
@@ -10,30 +10,22 @@
                                 <div class="post-card post-card--layout--list post-card--size--nl">
                                     <div class="post-card__image"><a href="view-news/<?=seo1($news['tieuDe'])?>/<?=$news['idTin'] ?>"><img src="<?=$news['hinhAnh']?>" alt=""></a></div>
                                     <div class="post-card__info">
-                                        <div class="post-card__category"><a href="#"></a>
-                                        </div>
+                                        <div class="post-card__category">Số lượt xem: <?=$news['luotXem']?></div>
                                         <div class="post-card__name"><a href="view-news/<?=seo1($news['tieuDe'])?>/<?=$news['idTin'] ?>"><?=$news['tieuDe']?></a></div>
                                         <div class="post-card__date"><?=date("d/m/y h:i:s", strtotime($news['ngayDang']))?></div>
                                         <div class="post-card__content"><?=$news['tomTat']?></div>
                                         <div class="post-card__read-more"><a href="view-news/<?=seo1($news['tieuDe'])?>/<?=$news['idTin'] ?>" class="btn btn-secondary btn-sm">ĐỌC THÊM</a></div>
                                     </div>
                                 </div>
+                            <hr>
                             </div>
                             <?php  }?>
                         </div>
                     </div>
                     <div class="posts-view__pagination">
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item disabled"><a class="page-link page-link--with-arrow" href="#" aria-label="Previous"><svg class="page-link__arrow page-link__arrow--left" aria-hidden="true" width="8px" height="13px">
-                                        <use xlink:href="images/sprite.svg#arrow-rounded-left-8x13"></use>
-                                    </svg></a></li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item active"><a class="page-link" href="#">2 <span class="sr-only">(current)</span></a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link page-link--with-arrow" href="#" aria-label="Next"><svg class="page-link__arrow page-link__arrow--right" aria-hidden="true" width="8px" height="13px">
-                                        <use xlink:href="images/sprite.svg#arrow-rounded-right-8x13"></use>
-                                    </svg></a></li>
-                        </ul>
+                        <?php   
+                            echo taoLinkPhanTrang($base_url, $total_rows, $page_num, $page_size);
+                        ?>
                     </div>
                 </div>
             </div>
@@ -41,58 +33,49 @@
         <div class="col-12 col-lg-4">
             <div class="block block-sidebar block-sidebar--position--end">
                 <div class="block-sidebar__item">
-                    <div class="widget-search">
-                        <form class="widget-search__body"><input class="widget-search__input" placeholder="Blog search..." type="text" autocomplete="off" spellcheck="false"> <button class="widget-search__button" type="submit"><svg width="20px" height="20px">
-                                    <use xlink:href="images/sprite.svg#search-20"></use>
-                                </svg></button></form>
-                    </div>
-                </div>
-                <div class="block-sidebar__item">
                     <div class="widget-aboutus widget">
-                        <h4 class="widget__title">About Blog</h4>
-                        <div class="widget-aboutus__text">Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget
-                            viverra nulla sem vitae neque. Quisque id sodales libero.</div>
+                        <h4 class="widget__title">Tin tức</h4>
+                        <div class="widget-aboutus__text">Cập nhật tin tức về công nghệ.</div>
                         <!-- social-links -->
                         <div class="social-links widget-aboutus__socials social-links--shape--rounded">
                             <ul class="social-links__list">
-                                <li class="social-links__item"><a class="social-links__link social-links__link--type--rss" href="#" target="_blank"><i class="fas fa-rss"></i></a></li>
-                                <li class="social-links__item"><a class="social-links__link social-links__link--type--youtube" href="#" target="_blank"><i class="fab fa-youtube"></i></a></li>
-                                <li class="social-links__item"><a class="social-links__link social-links__link--type--facebook" href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                                <li class="social-links__item"><a class="social-links__link social-links__link--type--twitter" href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                                <li class="social-links__item"><a class="social-links__link social-links__link--type--instagram" href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                <li class="social-links__item"><a class="social-links__link social-links__link--type--youtube" href="https://www.youtube.com/channel/UCTSMRdLqIiNNCLLdQ6RJEFw" target="_blank"><i class="fab fa-youtube"></i></a></li>
+                                <li class="social-links__item"><a class="social-links__link social-links__link--type--facebook" href="https://www.facebook.com/adphcbook" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
                             </ul>
                         </div><!-- social-links / end -->
                     </div>
                 </div>
+                <?php $lastestBlog = getLastestBlogs() ?>
                 <div class="block-sidebar__item">
                     <div class="widget-posts widget">
-                        <h4 class="widget__title">Latest Posts</h4>
+                        <h4 class="widget__title">Bài viết mới</h4>
                         <div class="widget-posts__list">
+                            <?php foreach($lastestBlog as $lastest_Blog){ ?>
                             <div class="widget-posts__item">
-                                <div class="widget-posts__image"><a href="#"><img src="images/posts/post-1-thumbnail.jpg" alt=""></a></div>
+                                <div class="widget-posts__image"><a href="view-news/<?=seo1($lastest_Blog['tieuDe'])?>/<?=$lastest_Blog['idTin'] ?>"><img src="<?=$lastest_Blog['hinhAnh'];?>" alt=""></a></div>
                                 <div class="widget-posts__info">
-                                    <div class="widget-posts__name"><a href="#">Philosophy That Addresses
-                                            Topics Such As Goodness</a></div>
-                                    <div class="widget-posts__date">October 19, 2019</div>
+                                    <div class="widget-posts__name"><a href="view-news/<?=seo1($lastest_Blog['tieuDe'])?>/<?=$lastest_Blog['idTin'] ?>"><?=$lastest_Blog['tieuDe'];?></a></div>
+                                    <div class="widget-posts__date"><?=date("d/m/y h:i:s", strtotime($lastest_Blog['ngayDang']))?></div>
                                 </div>
                             </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+                <?php $mostViewedBlogs = getMostViewedBlogs() ?>
+                <div class="block-sidebar__item">
+                    <div class="widget-posts widget">
+                        <h4 class="widget__title">Bài viết được xem nhiều</h4>
+                        <div class="widget-posts__list">
+                            <?php foreach($mostViewedBlogs as $most_Viewed_Blogs){ ?>
                             <div class="widget-posts__item">
-                                <div class="widget-posts__image"><a href="#"><img src="images/posts/post-2-thumbnail.jpg" alt=""></a></div>
+                                <div class="widget-posts__image"><a href="view-news/<?=seo1($most_Viewed_Blogs['tieuDe'])?>/<?=$most_Viewed_Blogs['idTin'] ?>"><img src="<?=$most_Viewed_Blogs['hinhAnh'];?>" alt=""></a></div>
                                 <div class="widget-posts__info">
-                                    <div class="widget-posts__name"><a href="#">Logic Is The Study Of
-                                            Reasoning And Argument Part 2</a></div>
-                                    <div class="widget-posts__date">September 5, 2019</div>
+                                    <div class="widget-posts__name"><a href="view-news/<?=seo1($most_Viewed_Blogs['tieuDe'])?>/<?=$most_Viewed_Blogs['idTin'] ?>"><?=$most_Viewed_Blogs['tieuDe'];?></a></div>
+                                    <div class="widget-posts__date"><?=date("d/m/y h:i:s", strtotime($most_Viewed_Blogs['ngayDang']))?></div>
                                 </div>
                             </div>
-                            <div class="widget-posts__item">
-                                <div class="widget-posts__image"><a href="#"><img src="images/posts/post-3-thumbnail.jpg" alt=""></a></div>
-                                <div class="widget-posts__info">
-                                    <div class="widget-posts__name"><a href="#">Some Philosophers Specialize
-                                            In One Or More Historical Periods</a></div>
-                                    <div class="widget-posts__date">August 12, 2019</div>
-                                </div>
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
